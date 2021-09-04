a7ae305976f4964484b7cc581f0f974b7790df8b
thuva9872 <64319324+thuva9872@users.noreply.github.com>
2021-09-04 22:35:14 +0530

    1

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 require('dotenv').config()
 
 const express = require('express')
-const cors = require('cors')
 const bcrypt=require('bcrypt');
 const server = express()
 const port = process.env.PORT || 8000
 
-server.use(cors())
 server.use(express.json())
 
 server.get('/', (req, res) => {
@@ -17,7 +15,11 @@ server.get('/hi', async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash("admin", salt);
     console.log(hashedPassword);
-    res.send('<h1>$hashassword</h1>');
+    //res.send('<h1>$hashassword</h1>');
+    res.json({
+        user:'Thuva',
+        access:'Denied'
+    });
 })
 
 server.listen(port, () => {
