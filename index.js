3f3296661de5f5fd78a7965d685389c4fc6c0bf8
thuva9872 <64319324+thuva9872@users.noreply.github.com>
2021-09-04 17:01:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ server.get('/', (req, res) => {
     res.send('<h1>This is a test application</h1>')
 })
 
+server.get('/hi', (req, res) => {
+    res.send('<h1>This is a testsnbjfskbhkfsjb application</h1>')
+})
+
 server.listen(port, () => {
     console.log(`\n=== Server listening on port ${port} ===\n`)
 })
