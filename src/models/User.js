fbc0c0fc5c87b37f08ff8cf5ede1ec48f9178b35
thuva9872 <64319324+thuva9872@users.noreply.github.com>
2021-09-13 09:21:29 +0530

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -81,7 +81,7 @@ class User {
         var userData= await this._database.readSingleTable(this._userType,['user_id','first_name','last_name','email','password','contact_no','is_active'],['email','=',this._email]);
         // console.log(userData.result);
         if (userData.error || !userData.result.rows[0]){
-            return new Promise((resolve)=> resolve({allowedAccess: false}));
+            return new Promise((resolve)=> resolve({allowedAccess: false,error:userData.error}));
         }
         userData=userData.result;
         this._u_id=userData.rows[0].user_id;
