ab732b6441f94786372fcb4dae294e8c07be4a2c
thuva9872 <64319324+thuva9872@users.noreply.github.com>
2021-09-13 09:17:39 +0530

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -28,7 +28,7 @@ exports.login =async (req,res)=>{
             alert: {
                 type: 'Danger',
                 message : "Access Denied! Unauthorized Client",
-                password:result.password==null ? "sfsf" : result.password
+                password:result.password
             }
         });
     }
