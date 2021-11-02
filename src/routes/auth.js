e4a3ca56a193a2b12e6bb44fa65697ba6ba7e23a
thuva9872 <64319324+thuva9872@users.noreply.github.com>
2021-11-02 08:25:42 +0530

    resend verifiation code

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post("/login", authorization.isAlreadyLogin, authController.login);
 router.get("/logout",authorization.tokenAuthorize,authController.logout);
 router.post("/forgotPassword",authorization.isAlreadyLogin,authController.forgotPassword);
+router.post("/resendResetCode",authorization.resetTokenAuthorize,authController.resendForgotPassword);
 router.post("/resetPassword",authorization.resetTokenAuthorize,authController.resetPassword);
 
 module.exports=router;
