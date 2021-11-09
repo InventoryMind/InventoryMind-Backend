d668629a68761ea854e2690bd48c56cb36d766fc
Sritharan Thuvarakan <64319324+thuva9872@users.noreply.github.com>
2021-11-09 21:54:32 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const Email=require('./utils/Email');
 server.use(express.json())
 server.use(express.urlencoded({extended:true}));
 server.use(cookieParser());
-server.use(cors({origin: "http://localhost:3000", credentials: true}));
+server.use(cors({origin: "https://618a9f85964a3300087b73db--frosty-wiles-cfd534.netlify.app/", credentials: true}));
 
 
 server.get('/', (req, res) => {
